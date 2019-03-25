a5f386b1cd0c17023a7420b46ee19da8c6f302f1
Ron Gierlach <rongierlach@gmail.com>
2019-03-24 19:44:00 -0700

    update core

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,18 +1,32 @@
-function withPlugins (Web3Class, pluginsArray) {
-  return class extends Web3Class {
+const validatePlugin = plugin => {
+  if (
+    typeof plugin !== 'object' ||
+    typeof plugin.addEndpoints !== 'function'
+  ) {
+    throw new Error('Web3 plugin must be an object with a field "addEndpoints" that is a function.')
+  }
+}
+
+function withPlugins (Web3, pluginsArray) {
+  return class Web3WithPlugins extends Web3 {
     constructor (...args) {
       super(...args)
 
+      // validate and apply each plugin
       for (let plugin of pluginsArray) {
-        if (
-          typeof plugin !== 'object' ||
-          typeof plugin.name !== 'string' ||
-          typeof plugin.factory !== 'function'
-        ) {
-          throw new Error('Web3 ')
-        }
+        // throw if plugin is not correctly configured
+        validatePlugin(plugin)
 
-        this[plugin.name] = plugin.factory(this);
+        // get the endpoints by calling the plugin's addEndpoints function
+        const endpoints = plugin.addEndpoints(this)
+        
+        // for each endpoint
+        // deeply set it on the web3 instance
+        // at the specified path
+        for (let path in endpoints) {
+          const endpoint = endpoints[path]
+          _.set(this, path, endpoint)
+        }
       }
     }
   }
