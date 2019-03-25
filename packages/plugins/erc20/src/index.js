af11299bf66b71d78863936cd488374fa8c50c73
Ron Gierlach <rongierlach@gmail.com>
2019-03-24 23:42:57 -0700

    update erc20 plugin

--- packages/plugins/erc20/src/index.js
+++ packages/plugins/erc20/src/index.js
@@ -1,6 +1,29 @@
+import abi from './abi'
+
+const contractFactoryFromABI = abi => (...args) => new web3.eth.Contract(abi, ...args)
+const withContractFactory = contractFactory => fn => (deployedAddr, ...args) => fn(contractFactory(deployedAddr), ...args)
+
 export default {
-  name: 'erc20',
-  factory: web3 => ({
-    
-  })
+  addEndpoints: web3 => {
+    const contractFactory = contractFactoryFromABI(abi)
+    const withContract = withContractFactory(contractFactory)
+
+    const contractMethodEndpoints = abi
+      .filter(interface => interface.type === 'function')
+      .reduce((endpoints, functionInterface) => {
+        const methodName = functionInterface.name
+        endpoints[methodName] = withContract((c, ...params) =>
+          c.methods[methodName](...params))
+
+        return endpoints
+      }, {})
+
+    return {
+      erc20: {
+        abi,
+        contractFactory,
+        ...contractMethodEndpoints
+      }
+    }
+  }
 }
